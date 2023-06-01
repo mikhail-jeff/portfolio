80bba557ba5c40002b8baa54edc88ef83ea21743
jeffmikhail <mikhailcruz78@gmail.com>
2023-06-01 21:03:23 +0800

    edited about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,11 +6,12 @@ const About = () => {
 			<div className='max-w-[1080px] mx-auto p-4'>
 				<p className='text-4xl font-bold text-center mb-4 text-gray-800'>About</p>
 				<p className='max-w-[800px] text-justify'>
-					An aspiring full stack web developer | software engineer equipped with a diverse and promising skill set, willing to be trained and improve, a team player who can thrive in any working environment and can work independently with minimal supervision.
+					I am an ambitious full stack web developer and software engineer with a versatile skill set, continuously seeking opportunities for growth and improvement. As a collaborative team player, I thrive in diverse working environments and possess the ability to work independently with minimal
+					supervision.
 				</p>
-				<div>
-					<p className=' py-2 mt-5 mb-3 text-xl font-bold text-center'>Educational Background</p>
-					<div className='flex justify-between items-center max-w-[500px] mx-auto'>
+				<div className='m-12'>
+					<p className=' py-1 mb-3 text-2xl font-bold text-center text-gray-800'>Educational Background</p>
+					<div className='flex justify-between items-center max-w-[600px] mx-auto'>
 						<p className='flex flex-col'>
 							<span className='font-bold'>Bachelor of Science in Information Technology</span>
 							<span className='font-medium'>- STI College Sta. Mesa -</span>
@@ -19,7 +20,7 @@ const About = () => {
 							<span className='font-medium'>2021</span>
 						</p>
 					</div>
-					<div className='flex justify-between items-center max-w-[500px] mx-auto mt-3'>
+					<div className='flex justify-between items-center max-w-[600px] mx-auto mt-3'>
 						<p className='flex flex-col'>
 							<span className='font-bold'>Full Stack Web Development Bootcamp</span>
 							<span className='font-medium'>- Kodego -</span>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,7 @@ const Contact = () => {
 	return (
 		<section
 			name='contact'
-			className='w-full h-screen flex flex-col justify-end items-center text-gray-500 mt-[260px]'
-		>
+			className='w-full h-screen flex flex-col justify-end items-center text-gray-500 mt-[260px]'>
 			<div className='max-w-[1080px] p-4 mx-auto w-full'>
 				<div>
 					<p className='text-center text-4xl font-bold text-gray-800'>Contact</p>
@@ -14,8 +13,7 @@ const Contact = () => {
 				<form
 					method='POST'
 					action='https://getform.io/f/2fc6803b-2078-4535-a217-cdde9c20e226'
-					className='flex flex-col max-w-[600px] w-full mx-auto'
-				>
+					className='flex flex-col max-w-[600px] w-full mx-auto'>
 					<input
 						className='my-1 p-2 bg-gray-300 focus:outline-none rounded-sm'
 						type='text'
@@ -35,8 +33,7 @@ const Contact = () => {
 						name='message'
 						id=''
 						rows='10'
-						placeholder='Write something...'
-					></textarea>
+						placeholder='Write something...'></textarea>
 					<button className='border p-2 my-1  bg-gray-300 hover:bg-gray-800 font-medium hover:text-white rounded-sm'>Submit</button>
 				</form>
 			</div>
